794d7c7059cf282e893af4aa3241cab0c9570f89
Wilfred007 <adzerwilfred007@gmail.com>
2025-02-26 00:32:08 +0100

    feat:Question tab completed

--- app/Components/FrequentlyAskedQuestions.tsx
+++ app/Components/FrequentlyAskedQuestions.tsx
@@ -28,13 +28,13 @@ export default function AnimatedTabs() {
     },
     {
       id: "tab3",
-      title: "Third Tab",
+      title: "Do I need crypto knowledge to use StreamFi?",
       content:
       "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
     },
     {
       id: "tab4",
-      title: "Fourth Tab",
+      title: " Is StreamFi free to use?",
       content:
       "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
     },
