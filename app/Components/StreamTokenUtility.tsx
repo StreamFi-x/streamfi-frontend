996cbf896b65e048e29b9b3e4a6131d3a8fb6d70
Chinedu Okeke <chiscookeke11@gmail.com>
2025-02-26 11:44:12 +0100

    feat: add stream token utility section

--- app/Components/StreamTokenUtility.tsx
+++ app/Components/StreamTokenUtility.tsx
@@ -40,10 +40,10 @@ export default function StreamTokenUtility() {
 
 
 
-<div className="md:w-[533.22px] w-[350px] md:h-[492px] ml-0 md:ml-auto mt-10 flex flex-col items-start justify-stretch gap-5 " >
+<div className="md:w-[533.22px] z-10 w-[350px] md:h-[492px] ml-0 md:ml-auto mt-10 flex flex-col items-start justify-stretch gap-5 " >
 
 {tokenUtilityData.map((item, index) => (
-    <div key={index} className=" w-full h-[108px] border-[1px] border-[#FFFFFF1A] rounded-lg bg-[#FFFFFF0D] p-6 flex flex-row items-start justify-start gap-8  " >
+    <div key={index} className="  w-full h-[108px] border-[1px] border-[#FFFFFF1A] rounded-lg bg-[#FFFFFF0D] p-6 flex flex-row items-start justify-start gap-8 transition-transform duration-300 hover:-translate-y-2 cursor-pointer  " >
 
     <div className=" w-12 h-12 rounded bg-[#007BFF1A] flex items-center justify-center " >
         <Image src={vpnKey} alt="vpn_key" height={24} width={24}  /> 
@@ -66,9 +66,35 @@ export default function StreamTokenUtility() {
 
 
 
+<div className="absolute top-[55%] left-[14%] flex items-center justify-center w-[200px] h-[150px] md:w-[449.8px] md:h-[304.2px] z-0  " >
+
+    <Image src={token1} alt="token1" height={100} width={100} className="object-cover h-full w-full "  />
+
+</div>
+<div className="absolute top-[35%] left-[7%] flex items-center justify-center w-[150px] h-[120px] md:w-[306px] md:h-[206px] blur-[2px]  " > 
+
+    <Image src={token2} alt="token2" height={100} width={100} className="object-cover h-full w-full "  />
+
+</div>
 
 
+<div className="absolute top-[20%] left-[28%] flex items-center justify-center w-[130px] h-[100px] md:w-[204px] md:h-[138px] blur-[3px]  " >
 
+    <Image src={token3} alt="token3" height={100} width={100} className="object-cover h-full w-full "  />
+
+</div>
+
+<div className="absolute top-[19%] left-[3%] flex items-center justify-center w-[150px] h-[120px] md:w-[204px] md:h-[138px] blur-[2px]  " >
+
+    <Image src={token4} alt="token4" height={100} width={100} className="object-cover h-full w-full "  />
+
+</div>
+
+<div className="absolute top-[5%] left-[10%] flex items-center justify-center w-[150px] h-[120px] md:w-[173px] md:h-[114px]  " >
+
+    <Image src={token5} alt="token5" height={100} width={100} className="object-cover h-full w-full "  />
+
+</div>
 
 
         </section>
