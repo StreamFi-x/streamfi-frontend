0e04c2fcfb12a9d0e3ecd65561788eb9e8485c9b
Emmzyemms <friyoemmy@gmail.com>
2025-03-24 14:34:07 +0100

    Update streamfi-logo/remove description Url

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,22 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
-
-
 export const metadata: Metadata = {
   title: {
     default: "Streamfi - Own Your Stream. Own Your Earnings",
     template: "%s - Streamfi",
   },
-  description:"Stream without limits, engage your community, and earn instantly with a blockchain-powered ecosystem that ensures true ownership, decentralized rewards, and frictionless transactions.",
+  description:
+    "Stream without limits, engage your community, and earn instantly with a blockchain-powered ecosystem that ensures true ownership, decentralized rewards, and frictionless transactions.",
 
   openGraph: {
     title: "Streamfi - Own Your Stream. Own Your Earnings",
     description:
       "Stream without limits, engage your community, and earn instantly with a blockchain-powered ecosystem that ensures true ownership.",
-    url: "http://192.168.137.1:3000",
     siteName: "Streamfi",
     images: [
       {
-        url: "http://192.168.137.1:3000/images/preview.jpg",
+        url: "http://192.168.137.1:3000/public/Images/streamFiLogo.svg",
         width: 1200,
         height: 630,
         alt: "Streamfi Preview",
@@ -33,7 +31,7 @@ export const metadata: Metadata = {
     title: "Streamfi - Own Your Stream. Own Your Earnings",
     description:
       "Stream without limits, engage your community, and earn instantly with a blockchain-powered ecosystem that ensures true ownership.",
-    images: ["http://192.168.137.1:3000/images/preview.jpg"],
+    images: ["http://192.168.137.1:3000/public/Images/streamFiLogo.svg"],
   },
 };
 
@@ -44,11 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={` antialiased`}
-      >
-        {children}
-      </body>
+      <body className={` antialiased`}>{children}</body>
     </html>
   );
 }
