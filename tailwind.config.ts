c02c3f5a3a07deba8e09146f3d6223fdcfd29bf5
legend4tech <ajuludennis14@gmail.com>
2025-03-26 21:16:48 +0100

    Resolve merge conflicts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,94 +1,44 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			'background-2': '#1C1C1C',
-  			'background-3': '#151515',
-  			foreground: 'hsl(var(--foreground))',
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			offWhite: '#F1F1F1',
-  			'offWhite-2': '#D9D9D9',
-  			grayish: '#CBCBCB',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		fontFamily: {
-  			helvetica: [
-  				'Helvetica Custom',
-  				'Arial',
-  				'sans-serif'
-  			]
-  		},
-  		spacing: {
-  			'8.5': '32px',
-  			'12.5': '50px',
-  			'23': '92px'
-  		},
-  		animation: {
-  			twinkle: 'twinkle 5s linear infinite',
-  			'twinkle-slow': 'twinkle 7s linear infinite',
-  			'twinkle-fast': 'twinkle 3s linear infinite'
-  		},
-  		keyframes: {
-  			twinkle: {
-  				'0%, 100%': {
-  					opacity: '0.2'
-  				},
-  				'50%': {
-  					opacity: '0.8'
-  				}
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "#17191A",
+        "background-2": "#1C1C1C",
+        "background-3": "#151515",
+        foreground: "var(--foreground)",
+        primary: "#5A189A",
+        offWhite: "#F1F1F1",
+        "offWhite-2": "#D9D9D9",
+        grayish: "#CBCBCB",
+      },
+      fontFamily: {
+        helvetica: ["Helvetica Custom", "Arial", "sans-serif"],
+        "pp-neue": ["PP Neue Machina", "sans-serif"],
+      },
+      spacing: {
+        "8.5": "32px",
+        "12.5": "50px",
+        "23": "92px",
+      },
+      animation: {
+        twinkle: "twinkle 5s linear infinite",
+        "twinkle-slow": "twinkle 7s linear infinite",
+        "twinkle-fast": "twinkle 3s linear infinite",
+      },
+      keyframes: {
+        twinkle: {
+          "0%, 100%": { opacity: "0.2" },
+          "50%": { opacity: "0.8" },
+        },
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config;
