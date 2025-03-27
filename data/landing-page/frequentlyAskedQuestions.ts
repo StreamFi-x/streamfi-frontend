d9d3c3005e482e77a02fc11d92dd344b3febf5f8
unknown <Chinonyerem661>
2025-03-27 11:01:40 +0100

    Modified FAQ section to Implement Mobile View & Responsiveness for FAQs Section

---
+++ data/landing-page/frequentlyAskedQuestions.ts
@@ -0,0 +1,42 @@
+// export const frequentlyAskedQuestions = [
+//     {
+//       id: "q1",
+//       title: "What is Stream_Fi?",
+//       content: "Stream_Fi is a decentralized streaming platform that enables seamless content sharing.",
+//     },
+//     {
+//       id: "q2",
+//       title: "How do I join?",
+//       content: "You can join by signing up on our website and connecting your wallet.",
+//     },
+//     {
+//       id: "q3",
+//       title: "Is Stream_Fi free to use?",
+//       content: "Yes! Basic features are free, but there are premium options for creators.",
+//     },
+//   ];
+  
+
+export const frequentlyAskedQuestions = [
+    {
+      id: "q1",
+      title: "What is StreamFi?",
+      content: "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
+    },
+    {
+      id: "q2",
+      title: "How do creators earn on StreamFi?",
+      content: "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
+    },
+    {
+      id: "q3",
+      title: "Do I need crypto knowledge to use StreamFi?",
+      content: "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
+    },
+    {
+        id: "q4",
+        title: "Is StreamFi free to use?",
+        content: "StreamFi is a decentralized live-streaming platform that allows creators to monetize their content without intermediaries. Powered by blockchain technology, it ensures 100% earnings retention, direct fan engagement, and community-driven governance.",
+      },
+  ];
+  
