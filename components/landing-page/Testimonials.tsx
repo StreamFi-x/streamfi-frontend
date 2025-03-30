405995a6fdf320a6251d24f4d1ca437d58f002c4
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-03-30 17:42:13 +0100

    refactor: improved the responsiveness of the waitlist component

--- components/landing-page/Testimonials.tsx
+++ components/landing-page/Testimonials.tsx
@@ -7,48 +7,50 @@ import "swiper/css";
 
 export default function Testimonials() {
   return (
-    <section className="xl:px-20 md:px-14 px-5 w-full max-w-5xl mx-auto mt-12 pb-8 flex flex-col gap-8 text-white">
-      <header className="w-full flex flex-col gap-3  items-center justify-center">
-        <h1 className="text-2xl sm:text-4xl xl:text-5xl font-extrabold font-pp-neue">
-          Don&apos;t just take our word for it
-        </h1>
-        <p className="text-white/80">
-          Hear from some of StreamFi amazing users{" "}
-        </p>
-      </header>
-      <Swiper
-        modules={[Autoplay]}
-        slidesPerView={1}
-        centeredSlides
-        spaceBetween={60}
-        freeMode
-        speed={1200}
-        autoplay={{
-          pauseOnMouseEnter: true,
-          disableOnInteraction: false,
-          delay: 2000,
-        }}
-        loop
-        breakpoints={{
-          768: {
-            slidesPerView: 3,
-          },
-        }}
-        className="w-full"
-      >
-        {testimonial_content.map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <Image
-              src={item.image}
-              width={0}
-              height={0}
-              alt=""
-              className="w-full h-full"
-              style={{ objectFit: "cover" }}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <section className=" w-full flex justify-center  text-white">
+      <div className="flex flex-col gap-8 container px-6 py-16 md:py-20 mx-auto  justify-center">
+        <header className="w-full flex flex-col gap-3  items-center justify-center max-w-5xl mx-auto">
+          <h1 className="text-2xl sm:text-4xl xl:text-5xl font-extrabold font-pp-neue ">
+            Don&apos;t just take our word for it
+          </h1>
+          <p className="text-white/80">
+            Hear from some of StreamFi amazing users{" "}
+          </p>
+        </header>
+        <Swiper
+          modules={[Autoplay]}
+          slidesPerView={1}
+          centeredSlides
+          spaceBetween={60}
+          freeMode
+          speed={1200}
+          autoplay={{
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+            delay: 2000,
+          }}
+          loop
+          breakpoints={{
+            768: {
+              slidesPerView: 3,
+            },
+          }}
+          className="w-full max-w-5xl"
+        >
+          {testimonial_content.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <Image
+                src={item.image}
+                width={0}
+                height={0}
+                alt=""
+                className="w-full h-full"
+                style={{ objectFit: "cover" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 }
