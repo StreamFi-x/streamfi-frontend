ee1c4eec9e9034b667f395757462219b96cfab04
Nwanne Nnamani <nnamaninwanne@gmail.com>
2025-04-29 01:47:17 +0100

    fix build error

--- components/settings/mob-nav.tsx
+++ components/settings/mob-nav.tsx
@@ -17,7 +17,7 @@ const MobNav = () => {
   ];
 
   // Check if route is active
-  const isActive = (path) => {
+  const isActive = (path: string) => {
     return pathname === path || (path !== '/' && pathname.startsWith(path));
   };
 
