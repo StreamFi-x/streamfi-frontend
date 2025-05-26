f46d584d729dbfc8b7156e22026d49ed29c00204
Taiwo shakirah <shakirattaiwo6@gmail.com>
2025-05-26 15:43:56 +0100

    feat implement category table

--- utils/upload/Dcloudinary.ts
+++ utils/upload/Dcloudinary.ts
@@ -110,8 +110,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
 });
 
 /**
