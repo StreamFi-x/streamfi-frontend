d3eb8b0059017e913d12b38241e7056bea32461f
sudiptapaul <psudipta858@gmail.com>
2025-06-02 19:26:44 +0530

    feat: add loading-state

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { FeaturedStream } from "@/components/explore/home/FeaturedStream";
 import { LiveStreams } from "@/components/explore/home/LiveStreams";
 import { TrendingStreams } from "@/components/explore/home/TrendingStreams";
 import { featuredStream } from "@/data/explore/home/featured-stream";
 import { liveStreams } from "@/data/explore/home/live-streams";
 import { trendingStreams } from "@/data/explore/home/trending-streams";
+import SimpleLoader from "@/components/ui/loader/simple-loader"
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 100); 
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoading) {
+    return <SimpleLoader />;
+  }
+
   return (
     <div className="min-h-screen bg-black text-white">
       <main className="container mx-auto px-4 py-8">
